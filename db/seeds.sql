4ee6d42c21ae9113a2d4065dab2eba5a6ee2e6cc
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-04-17 00:11:21 -0700

    attempt

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('BigMac');
+INSERT INTO burgers (burger_name) VALUES ('CheeseKing');
+INSERT INTO burgers (burger_name) VALUES ('KinderBurger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('McBacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('ChickenDelice', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('FattyB', true);
